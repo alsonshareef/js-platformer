bc4bcc4f683d5e4c17940502d3452f14fc817c96
Alson Shareef <alson.shareef@gmail.com>
2020-02-01 22:10:39 +0800

    General webpack config for both development and production

---
+++ webpack.common.js
@@ -0,0 +1,23 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/js/index.js',
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist'
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'JavaScript Platformer',
+      template: './src/template.html'
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  }
+};
