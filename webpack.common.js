a9d74324c05e3159230248a4669d11dd8d792232
Alson Shareef <alson.shareef@gmail.com>
2020-02-01 23:48:58 +0800

    Added babel-loader with plugin and presets.

--- webpack.common.js
+++ webpack.common.js
@@ -17,6 +17,17 @@ module.exports = {
       {
         test: /\.css$/,
         use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+            plugins: ['@babel/plugin-proposal-class-properties']
+          }
+        }
       }
     ]
   }
