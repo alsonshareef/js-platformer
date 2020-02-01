41f0f13fedf27bfdcee8768e79a5ab1950f60701
Alson Shareef <alson.shareef@gmail.com>
2020-02-01 22:11:06 +0800

    Production specific webpack config.

---
+++ webpack.prod.js
@@ -0,0 +1,15 @@
+const path = require('path');
+const common = require('./webpack.common');
+const merge = require('webpack-merge');
+
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = merge(common, {
+  mode: 'production',
+  output: {
+    filename: 'bundle.[contentHash].js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  devtool: 'inline-source-map',
+  plugins: [new CleanWebpackPlugin()]
+});
