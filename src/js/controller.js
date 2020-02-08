7234d4ef637aafcee79071908fbb2fdb615de42b
Alson Shareef <alson.shareef@gmail.com>
2020-02-08 20:04:50 +0800

    Controller class for handling user input

---
+++ src/js/controller.js
@@ -0,0 +1,30 @@
+/**
+ * This file is responsible for storing all logic related to user input.
+ */
+
+export default class Controller {
+  constructor() {
+    this.active = {
+      left: false,
+      right: false,
+      up: false
+    };
+  }
+
+  // Handles state of keypresses for character movement.
+  directionHandler = (type, keycode) => {
+    let keyDown = type === 'keydown' ? true : false;
+
+    switch (keycode) {
+      case 37:
+        this.active.left = keyDown;
+        break;
+      case 38:
+        this.active.up = keyDown;
+        break;
+      case 39:
+        this.active.right = keyDown;
+        break;
+    }
+  };
+}
