adeff854e5588dd77254064bce432656ec57b1b1
Alson Shareef <alson.shareef@gmail.com>
2020-02-09 18:30:49 +0800

    Display class which stores logic of canvas and drawing of the game world.

---
+++ src/js/display.js
@@ -0,0 +1,20 @@
+export default class Display {
+  constructor(canvas) {
+    this.ctx = canvas.getContext('2d');
+    this.ctx.canvas.height = 180;
+    this.ctx.canvas.width = 320;
+  }
+
+  // Draw the player
+  draw(x, y, width, height, color) {
+    // Player
+    this.ctx.fillStyle = color;
+    this.ctx.fillRect(Math.floor(x), Math.floor(y), width, height);
+  }
+
+  // Fill in the world.
+  fill(color) {
+    this.ctx.fillStyle = color;
+    this.ctx.fillRect(0, 0, 320, 180);
+  }
+}
