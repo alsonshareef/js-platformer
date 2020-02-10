9d4e3b680cfe085efdda056bb7075c2f55a58665
Alson Shareef <alson.shareef@gmail.com>
2020-02-10 20:57:39 +0800

    method order swap

--- src/js/display.js
+++ src/js/display.js
@@ -5,16 +5,16 @@ export default class Display {
     this.ctx.canvas.width = 320;
   }
 
+  // Fill in the world.
+  fill(color) {
+    this.ctx.fillStyle = color;
+    this.ctx.fillRect(0, 0, 320, 180);
+  }
+
   // Draw the player
   draw(x, y, width, height, color) {
     // Player
     this.ctx.fillStyle = color;
     this.ctx.fillRect(Math.floor(x), Math.floor(y), width, height);
-  }
-
-  // Fill in the world.
-  fill(color) {
-    this.ctx.fillStyle = color;
-    this.ctx.fillRect(0, 0, 320, 180);
   }
 }
