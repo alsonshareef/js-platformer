a017d2f3ffd841adcaf73a1e1d8a79727e92320a
Alson Shareef <alson.shareef@gmail.com>
2020-02-11 22:49:28 +0800

    Specific resize method responsible for canvas dimension changes which will be invoked in main resize game function.

--- src/js/display.js
+++ src/js/display.js
@@ -10,10 +10,28 @@ export default class Display {
     this.ctx.canvas.width = 320;
   }
 
+  // Resize game canvas on viewport size changes to dimensions with game world aspect ratio.
+  resize = (canvasWidth, canvasHeight, aspectRatio) => {
+    if (canvasHeight / canvasWidth > aspectRatio) {
+      // Max width scenario
+      this.ctx.canvas.height = canvasWidth * aspectRatio;
+      this.ctx.canvas.width = canvasWidth;
+    } else {
+      // Max height scenario
+      this.ctx.canvas.height = canvasHeight;
+      this.ctx.canvas.width = canvasHeight / aspectRatio;
+    }
+
+    return {
+      width: this.ctx.canvas.width,
+      height: this.ctx.canvas.height
+    };
+  };
+
   // Fill in the world.
   fill(color) {
     this.ctx.fillStyle = color;
-    this.ctx.fillRect(0, 0, 320, 180);
+    this.ctx.fillRect(0, 0, this.ctx.canvas.width, this.ctx.canvas.height);
   }
 
   // Draw the player
