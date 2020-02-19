671396a005b438c181402a9c13d0f850acf76c4b
Alson Shareef <alson.shareef@gmail.com>
2020-02-19 14:34:50 +0800

    Reduced new canvas dimensions by 10% to add more white space around the canvas

--- src/js/display.js
+++ src/js/display.js
@@ -4,22 +4,23 @@
  */
 
 export default class Display {
-  constructor(canvas) {
+  constructor(canvas, levelData) {
     this.ctx = canvas.getContext('2d');
     this.ctx.canvas.height = 180;
     this.ctx.canvas.width = 320;
+    this.level = new Level(levelData);
   }
 
   // Resize game canvas on viewport size changes to dimensions with game world aspect ratio.
   resize = (canvasWidth, canvasHeight, aspectRatio) => {
     if (canvasHeight / canvasWidth > aspectRatio) {
       // Max width scenario
-      this.ctx.canvas.height = canvasWidth * aspectRatio;
-      this.ctx.canvas.width = canvasWidth;
+      this.ctx.canvas.height = canvasWidth * aspectRatio * 0.9;
+      this.ctx.canvas.width = canvasWidth * 0.9;
     } else {
       // Max height scenario
-      this.ctx.canvas.height = canvasHeight;
-      this.ctx.canvas.width = canvasHeight / aspectRatio;
+      this.ctx.canvas.height = canvasHeight * 0.9;
+      this.ctx.canvas.width = (canvasHeight / aspectRatio) * 0.9;
     }
 
     return {
@@ -39,5 +40,11 @@ export default class Display {
     // Player
     this.ctx.fillStyle = color;
     this.ctx.fillRect(Math.floor(x), Math.floor(y), width, height);
+  }
+}
+
+class Level {
+  constructor(levelData) {
+    this.levelData = levelData;
   }
 }
