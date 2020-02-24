0243a67ef518aade93ccc93e65034d06d1e9e1a5
Alson Shareef <alson.shareef@gmail.com>
2020-02-24 21:22:55 +0800

    Level class was created to build out a grid representation of element instances which has state and methods which will be manipulated on user input. Game update methods now refer to player instance within Level instance.

--- src/js/game.js
+++ src/js/game.js
@@ -1,24 +1,28 @@
 /**
  * This file stores the Game class which is responsible for storing data and methods related
- * to world of the game and it's state, as well as the Player class which is responsible for
- * storing data and methods related to the player object drawn to the canvas and it's state.
+ * to world of the game and it's state. Within the Game class is an instance of the Level class
+ * which is given the level data passed in from the JSON file. The Level class will then build
+ * the level out as an array of arrays of elements of the game as well as store other data about
+ * the level such as height and width of the grid. The built out level array will store instances
+ * of dynamic elements such as the Player, Coins and Lava and these instances store extra
+ * functionality for updating their state based on user input.
  */
 
 export default class Game {
-  constructor() {
+  constructor(levelData) {
+    this.level = new Level(levelData);
     this.world = {
       height: 180,
       width: 320,
       background_color: '#202020',
-      player: new Player(),
       gravity: 1.5,
       friction: 0.9,
 
       handleCollision: player => {
         // if character is falling below floor line
-        if (player.y > this.world.height - this.world.player.height) {
+        if (player.y > this.world.height - player.height) {
           player.jumping = false;
-          player.y = this.world.height - this.world.player.height;
+          player.y = this.world.height - player.height;
           player.y_velocity = 0;
         }
 
@@ -38,19 +42,64 @@ export default class Game {
         this.world.height = height;
       },
 
-      update: function() {
-        this.player.y_velocity += this.gravity; // gravity
-        this.player.x_velocity *= this.friction; // friction
-        this.player.y_velocity *= this.friction; // friction
-        this.player.update();
-        this.handleCollision(this.player);
+      update: () => {
+        this.level.player[0].y_velocity += this.world.gravity; // gravity
+        this.level.player[0].x_velocity *= this.world.friction; // friction
+        this.level.player[0].y_velocity *= this.world.friction; // friction
+        this.level.player[0].update();
+        this.world.handleCollision(this.level.player[0]);
       }
     };
   }
 }
 
+class Level {
+  constructor(levelData) {
+    this.levelData = levelData.test;
+    this.height = this.levelData.length;
+    this.width = this.levelData[0].length;
+    this.elementTypes = {
+      '@': Player,
+      '=': Lava,
+      '0': Coin,
+      '#': 'wall',
+      '+': 'lava',
+      '.': 'empty'
+    };
+    this.movingElements = [];
+    this.stationaryElements = this.levelData.map((row, y) => {
+      return row.map((element, x) => {
+        // Filter out stationary elements
+        let elementClass = this.elementTypes[element];
+        if (typeof elementClass === 'string') {
+          return elementClass;
+        }
+        // Remaining moving elements are instantiated and pushed into moving elements array.
+        this.movingElements.push(new elementClass(new Element(x, y), element));
+        return 'empty';
+      });
+    });
+    this.player = this.movingElements.filter(element => {
+      return element.type === 'player';
+    });
+  }
+}
+
+/**
+ * DYNAMIC ELEMENT CLASSES
+ */
+
+class Element {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+  }
+}
+
 class Player {
-  constructor() {
+  constructor(position) {
+    this.type = 'player';
+    this.position = position;
     this.color = 'red';
     this.height = 32;
     this.width = 32;
@@ -84,9 +133,19 @@ class Player {
     this.width = canvasWidth / 32;
     this.height = canvasWidth / 32;
   };
+}
 
-  updateJumpHeight = canvasHeight => {
-    this.jumpHeight = -canvasHeight / 30;
-    console.log(this.jumpHeight);
-  };
+class Coin {
+  constructor(position) {
+    this.type = 'coin';
+    this.position = position;
+  }
+}
+
+class Lava {
+  constructor(position, element) {
+    this.type = 'lava';
+    this.position = position;
+    this.element = element;
+  }
 }
